cd1e300d601da899a3d26471808d70b9f1c4fcdf
comeheral <comeheral@gmail.com>
2026-04-06 17:09:55 +0200

    habits creation modal : handle modal close, form reset and success, add form error label

--- inertia/pages/habits/index.tsx
+++ inertia/pages/habits/index.tsx
@@ -1,12 +1,30 @@
 import type { InertiaProps } from '~/types'
 import type { Data } from '@generated/data'
+import { toast } from 'sonner'
+import { useRef } from 'react'
 import { Form } from '@adonisjs/inertia/react'
+import { FormComponentRef } from '@inertiajs/core'
 
 type PageProps = InertiaProps<{
   habits: Data.Habit[]
 }>
 
 export default function Index({ habits }: PageProps) {
+  const formRef = useRef<FormComponentRef>(null)  // Allows to access Form component's methods
+
+  const closeModal = () => {
+    (document.getElementById('add_modal') as HTMLDialogElement)?.close();  // Close the modal
+
+    setTimeout(() => {
+      formRef.current?.reset();  // Reset the form (after modal animation)
+    }, 300)
+  }
+
+  const handleSuccess = () => {
+    closeModal();
+    toast.success('New habit added!');
+  }
+
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">Habits</h1>
@@ -22,12 +40,13 @@ export default function Index({ habits }: PageProps) {
       <dialog id="add_modal" className="modal">
         <div className="modal-box">
           <h2 className="font-bold text-lg mb-4">Add a habit</h2>
-          <Form route="habits.store" options={{ preserveScroll: true, preserveState: true, preserveUrl: true}}>
+          <Form ref={formRef} route="habits.store" options={{ preserveScroll: true, preserveState: true, preserveUrl: true}} onSuccess={handleSuccess}>
             {({ errors }) => (
               <>
-                <input type="text" name="name" id="name" placeholder="Habit name" className="input mb-4" />
-                <div className="flex gap-2">
-                  <button type="button" className="btn btn-tertiary w-100 shrink" onClick={()=>(document.getElementById('add_modal') as HTMLDialogElement)?.close()}>Cancel</button>
+                <input type="text" name="name" id="name" placeholder="Habit name" required className="input" />
+                {errors.name && <p className="text-sm text-center text-error mt-2">{errors.name}</p>}
+                <div className="flex gap-2 mt-4">
+                  <button type="button" className="btn btn-tertiary w-100 shrink" onClick={closeModal}>Cancel</button>
                   <button type="submit" className="btn btn-primary w-100 shrink">Add</button>
                 </div>
               </>
